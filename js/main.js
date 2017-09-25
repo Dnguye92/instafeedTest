3443e2d8bd5a998166dc7c3b5082900582fcc75b
Dylan Nguyen <Dnguye92@yahoo.com>
2017-09-24 19:00:09 -0700

    fixed value inside anchor tag - agnific popup is now working with instafeed

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 var feed = new Instafeed({
     get: 'user',
     userId: '34554451',
-    limit: 12,
+    limit: 20,
     resolution: 'standard_resolution',
     accessToken: '34554451.1677ed0.cfab4b35a3574749a2ec82d7e5fdc261',
     sortBy: 'most-recent',
     clientId: '624480682e654f5f93e8e563a0b3c37a',
-    template: '<div class="col-xs-4"><a href="{{link}}" target="_blank"><img src="{{image}}" class="img-responsive instagram-img"/></a></div>'
+    template: '<div class="col-xs-4"><a href="{{image}}" title="{{caption}}"target="_blank"><img src="{{image}}" class="img-responsive instagram-img"/></a><p>{{caption}}</p></div>'
 });
 
 feed.run();
