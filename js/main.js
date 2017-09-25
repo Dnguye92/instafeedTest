a62e77ebcc9a2ebfc5e66493cc95ac900f22d1a8
Dylan Nguyen <Dnguye92@yahoo.com>
2017-09-24 20:03:30 -0700

    added new method to load more instagram images on click

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var loadBtn = document.getElementById('loadBtn')
+
 var feed = new Instafeed({
     get: 'user',
     userId: '34554451',
@@ -6,7 +8,16 @@ var feed = new Instafeed({
     accessToken: '34554451.1677ed0.cfab4b35a3574749a2ec82d7e5fdc261',
     sortBy: 'most-recent',
     clientId: '624480682e654f5f93e8e563a0b3c37a',
-    template: '<div class="col-xs-4"><a href="{{image}}" title="{{caption}}"target="_blank"><img src="{{image}}" class="img-responsive instagram-img"/></a><p>{{caption}}</p></div>'
+    template: '<div class="col-xs-4"><a href="{{image}}" title="{{caption}}"target="_blank"><img src="{{image}}" class="img-responsive instagram-img"/></a><p>{{caption}}</p></div>',
+    after: function() {
+        if(!this.hasNext()) {
+            loadBtn.setAttribute('disabled', 'disabled');
+        }
+    }
+});
+
+loadBtn.addEventListener('click', function() {
+  feed.next();
 });
 
 feed.run();
